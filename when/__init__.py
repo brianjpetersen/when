db371769a232c2fafbc8faf0159c8989c372592c
Brian J Petersen <brianjpetersen@gmail.com>
2015-06-30 13:45:50 -0400

    .

--- when/__init__.py
+++ when/__init__.py
@@ -8,12 +8,12 @@ from . import (when, )
 
 __all__ = ('__version__', 'now', 'is_timezone_aware', 'When', )
 
-_where = os.path.dirname(os.path.abspath(__file__))
+__where__ = os.path.dirname(os.path.abspath(__file__))
 
-with open(os.path.join(_where, '..', 'VERSION'), 'rb') as f:
+with open(os.path.join(__where__, '..', 'VERSION'), 'rb') as f:
     __version__ = f.read()
 
 
 When = when.When
+Whence = when.Whence
 now = when.now
-is_timezone_aware = when.is_timezone_aware
